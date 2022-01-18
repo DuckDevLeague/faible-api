cff1a3a51c94a82ce474c60ab42fdd1e569b9b6c
peterhttps <plrf100@gmail.com>
2022-01-18 20:58:47 -0300

    ci(git): removing dist folder

--- dist/routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-var express_1 = require("express");
-var stories_1 = require("./routes/stories");
-var router = (0, express_1.Router)();
-exports.router = router;
-router.get('/story/:id', stories_1.getStoryById);
