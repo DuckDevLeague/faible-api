4028e36e90fb941919f610fd99fbfa816bd5af60
peterhttps <plrf100@gmail.com>
2022-01-19 22:27:55 -0300

    fix(logs): removing logs

--- src/routes/stories.ts
+++ src/routes/stories.ts
@@ -46,7 +46,7 @@ export const getStoryById = async (req: Request, res: Response) => {
   const { id } = req.params;
   const storiesRef = db.collection('stories');
   const story = await storiesRef.doc(id).get();
-  console.log('log');
+
   if (story.exists) {
     const dataResponse = {
       ...story.data(),
